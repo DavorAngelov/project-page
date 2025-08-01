bba2d8fb12db74706d6086cbb397586704a8c13e
DavorAngelov <ddavor.angelov@gmail.com>
2025-08-01 02:05:11 +0200

    added navlink to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
+    const baseClass = 'hover:text-blue-600 transition-colors cursor-pointer pb-1'
+
+    const activeClass = 'border-b-2 border-blue-600 font-semibold'
+
     return (
-        <header className="flex justify-between items-center p-6 lg:px-12 rounded-2xl shadow-lg shadow-blue-200 mb-3 bg-white mt-1">
+        <header
+            className="flex justify-between items-center p-6 lg:px-12 rounded-2xl shadow-lg shadow-blue-200 mb-3 bg-white mt-1">
             <div className="text-2xl font-bold tracking-wider text-blue-600">CivilEngML</div>
             <nav className="hidden md:flex space-x-8">
-                <a href="#" className="hover:text-blue-600  transition-colors cursor-pointer">Дома</a>
-                <a href="#" className="hover:text-blue-600 transition-colors cursor-pointer">Анализи / Проекти</a>
-                <a href="#" className="hover:text-blue-600 transition-colors cursor-pointer">Едукација</a>
-                <a href="#" className="hover:text-blue-600 transition-colors cursor-pointer">За нас</a>
+                <NavLink to="/" className={({isActive}) => `${baseClass} ${isActive ? activeClass : ''}`}>
+                    Дома
+                </NavLink>
+                <NavLink to="/projects" className={({isActive}) => `${baseClass} ${isActive ? activeClass : ''}`}>
+                    Анализи / Проекти
+                </NavLink>
+                <NavLink to="/education" className={({isActive}) => `${baseClass} ${isActive ? activeClass : ''}`}>
+                    Едукација
+                </NavLink>
+                <NavLink to="/about" className={({isActive}) => `${baseClass} ${isActive ? activeClass : ''}`}>
+                    За нас
+                </NavLink>
             </nav>
-            <button className="border border-gray-500 px-4 py-2 rounded hover:text-blue-600 transition-colors cursor-pointer">
+            <NavLink to="/contact" className="border border-gray-500 px-4 py-2 rounded hover:text-blue-600 transition-colors cursor-pointer">
                 Контакт
-            </button>
+            </NavLink>
         </header>
     )
 }
+
 export default Navbar
